1b2aae6843b005fba1834523ee9ed74c9a006c6f
reham <reham.tarek.inbox@gmail.com>
2024-03-24 13:22:34 +0200

    splash and theming

--- lib/theme/appTheme.dart
+++ lib/theme/appTheme.dart
@@ -1,40 +1,40 @@
-// import 'package:flutter/material.dart';
-//
-// class MyTheme{
-//   static Color primaryLightColor=Color(0xff39A552);
-//   static Color whiteColor=Color(0xfffffff);
-//   static Color redColor=Color(0xffC21C22);
-//   static Color darkBlueColor=Color(0xff003E90);
-//   static Color pinkColor=Color(0xffED1E79);
-//   static Color noMovieColor=Color(0xffB5B4B4);
-//   static Color greyBottomColor=Color(0xff4882cF);
-//   static Color yellowColor=Color(0xffF2D352);
-//   static Color greyColor=const Color(0xff514F4F);
-//   static Color blackColor=Color(0xff1E1E1E);
-//
-//
-//   static ThemeData lightTheme=ThemeData(
-//       primaryColor: primaryLightColor,
-//       appBarTheme: AppBarTheme(
-//           backgroundColor: primaryLightColor,
-//           centerTitle: true,
-//           shape: const RoundedRectangleBorder(
-//               borderRadius: BorderRadius.only(
-//                 bottomLeft:Radius.circular(30) ,
-//                 bottomRight:Radius.circular(30) ,
-//               ))),
-//
-//       textTheme: TextTheme(
-//         titleLarge: TextStyle(
-//             fontSize: 22,fontWeight: FontWeight.bold,color: whiteColor),
-//
-//
-//         titleMedium: TextStyle(
-//             fontSize: 20,fontWeight: FontWeight.bold,color: blackColor),
-//
-//
-//         titleSmall: TextStyle(
-//             fontSize: 18,fontWeight: FontWeight.bold,color: blackColor),
-//       ) );
-//
-// }
+import 'package:flutter/material.dart';
+
+class MyTheme{
+  static Color whiteColor=Color(0xffC6C6C6);
+  static Color noMovieColor=Color(0xffB5B4B4);
+  static Color yellowColor=Color(0xffFFB224);
+  static Color greyColor=const Color(0xff514F4F);
+  static Color blackColor=Color(0xff1E1E1E);
+
+
+  static ThemeData lightTheme=ThemeData(
+      primaryColor: whiteColor,
+      scaffoldBackgroundColor:Colors.transparent,
+      appBarTheme:  AppBarTheme(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          centerTitle: true,
+          iconTheme: IconThemeData(
+            color:whiteColor,
+          )
+      ),
+      bottomNavigationBarTheme: BottomNavigationBarThemeData(
+        selectedItemColor: yellowColor,
+        showUnselectedLabels: true,
+      ),
+
+      textTheme: TextTheme(
+        titleLarge: TextStyle(
+            fontSize: 22,fontWeight: FontWeight.bold,color: whiteColor),
+
+
+        titleMedium: TextStyle(
+            fontSize: 20,color: blackColor,fontWeight: FontWeight.w400),
+
+
+        titleSmall: TextStyle(
+            fontSize: 18,color: blackColor,fontWeight: FontWeight.w300),
+      ) );
+
+}
