bc6715693e66ebe6b355031e02c6efd9718b7e17
reham <reham.tarek.inbox@gmail.com>
2024-03-26 02:15:28 +0200

    splash screen and bottom navigation bar

---
+++ lib/presentation/browse/view/browse_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class BrowseView extends StatelessWidget {
+  const BrowseView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
